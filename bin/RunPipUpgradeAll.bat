d49c17aa5f31a63ae0e68be5ce653c0c592437c7
Kay Hau <virtualda@gmail.com>
2018-04-20 20:59:14 +1000

    Delete RunPipUpgradeAll.bat

--- bin/RunPipUpgradeAll.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-pip install -U pip
-pip freeze > requirements_win_27.txt && pip install --upgrade -r requirements_win_27.txt && pip3 freeze > requirements_win_27.txt
-
-pip3 install -U pip
-pip3 freeze > requirements_win_36.txt && pip3 install --upgrade -r requirements_win_36.txt && pip3 freeze > requirements_win_36.txt
