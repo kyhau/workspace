a30388c2f9b33cca76f08f508affd7c2f94152e4
Kay Hau <virtualda@gmail.com>
2019-05-26 19:26:33 +1000

    Update python installation script

--- python/setup_python.sh
+++ python/setup_python.sh
@@ -2,30 +2,30 @@
 
 # Run as root
 
-# Install Python 2.7, 3.6, 3.7
+apt update && apt-get install -y software-properties-common
+add-apt-repository -y ppa:deadsnakes/ppa
+
+# Install Python 3.7, 3.8
 apt-get install -y \
-    python-minimal \
-    python3-minimal \
-    python2.7-dev \
-    python3.6-dev \
     python3.7-dev \
+    python3.8-dev \
+    python3.8-distutils \
     && apt-get -y clean
 
-pushd /opt/
+cd /opt/
 
 # Install pip via PyPA's recommended way rather than the outdated apt repos
 # See: https://pip.pypa.io/en/stable/installing/
 curl https://bootstrap.pypa.io/get-pip.py -o ./get-pip.py && \
-    python2.7 get-pip.py && \
-    python3.6 get-pip.py && \
-    python3.7 get-pip.py
+    python3.8 get-pip.py && \
+    python3.7 get-pip.py && \
+
 rm /opt/get-pip.py
 popd
 
 # Upgrade pip and install virtualenv
-python2.7 -m pip install -U pip virtualenv && \
-python3.6 -m pip install -U pip virtualenv && \
-python3.7 -m pip install -U pip virtualenv
+python3.8 -m pip install -U pip virtualenv virtualenvwrapper wheel
+python3.7 -m pip install -U pip virtualenv virtualenvwrapper wheel
 
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 python --version
