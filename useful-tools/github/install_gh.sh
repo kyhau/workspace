7b6063e2cb74d3ce34fb87c58a512e103e39362d
Kay Hau <virtualda@gmail.com>
2021-08-26 16:38:30 +1000

    Updated install_gh.sh

--- useful-tools/github/install_gh.sh
+++ useful-tools/github/install_gh.sh
@@ -2,7 +2,10 @@
 # See also https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 set -e
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
 
 # If seeing error
 # ImportError: cannot import name '_gi' from partially initialized module 'gi' (most likely due to a circular import)
@@ -12,6 +15,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB9
 #sudo apt install libgirepository1.0-dev -y
 #sudo python3.8 -m pip install --ignore-installed PyGObject
 
-sudo apt-add-repository https://cli.github.com/packages
+#sudo apt-add-repository https://cli.github.com/packages
 sudo apt update
 sudo apt install gh
