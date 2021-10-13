75eddba33e6297881a01ccc02eebbc5b61256add
Kay Hau <virtualda@gmail.com>
2021-10-13 21:04:27 +1100

    useful-tools/python/run_pip_upgrade_all.sh

--- useful-tools/python/run_pip_upgrade_all.sh
+++ useful-tools/python/run_pip_upgrade_all.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 pip install -U pip
-pip freeze > tmp_requirements.txt && pip install -U -r tmp_requirements.txt && rm tmp_requirements.txt
+pip list --outdated --format=freeze | grep -v '^\-e' | grep -vi PyGObject | cut -d = -f 1  | xargs -n1 pip install -U
+
+#pip freeze > tmp_requirements.txt && pip install -U -r tmp_requirements.txt && rm tmp_requirements.txt
