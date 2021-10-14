4edcf6f99b26d7b9f91fa43e31d3d3847feb84ca
kyhau <virtualda@gmail.com>
2021-10-14 18:16:41 +1100

    Updated degreen

--- ubuntu/degreen.sh
+++ ubuntu/degreen.sh
@@ -3,7 +3,7 @@
 find . -type f ! -iname "*.bat" ! -iname "*.exe" ! -iname "*.js" ! -iname "*.ps1" ! -iname "*.sh" ! -iname "*.ts" \
   -not -path "*.aws-sam/*" \
   -not -path "*.build/*" \
-  -not -path "*.git*" \
+  -not -path "*.git/*" \
   -not -path "*.idea/*" \
   -not -path "*.pytest_cache/*" \
   -not -path "*.tox/*" \
