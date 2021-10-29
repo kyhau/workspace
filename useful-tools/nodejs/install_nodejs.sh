131022650045b60fdc489f5d093e26a05ffdf765
kyhau <virtualda@gmail.com>
2021-10-29 19:05:44 +1100

    Updated useful-tools/nodejs/install_nodejs.sh

--- useful-tools/nodejs/install_nodejs.sh
+++ useful-tools/nodejs/install_nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # See also https://github.com/nodesource/distributions#debmanual
 
-VERSION=15.x
+VERSION=16.x
 
 # Remove current installations, if any
 #sudo apt-get --purge remove node
@@ -14,15 +14,13 @@ curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key --insecure | sudo
 ####################################################################################
 # https://github.com/nodesource/distributions#manual-installation
 
-# Replace with the branch of Node.js or io.js you want to install: node_6.x, node_8.x, etc...
-VERSION=node_15.x
 # The below command will set this correctly, but if lsb_release isn't available, you can set it manually:
 # - For Debian distributions: jessie, sid, etc...
 # - For Ubuntu distributions: xenial, bionic, etc...
 # - For Debian or Ubuntu derived distributions your best option is to use the codename corresponding to the upstream release your distribution is based off. This is an advanced scenario and unsupported if your distribution is not listed as supported per earlier in this README.
 DISTRO="$(lsb_release -s -c)"
-echo "deb http://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src http://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+echo "deb http://deb.nodesource.com/node_$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src http://deb.nodesource.com/node_$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 
 sudo apt-get update && sudo apt-get install nodejs
 
