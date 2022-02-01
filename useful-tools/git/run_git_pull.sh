462e25b18625a5d32206c3012339fdddbb0e4e00
Kay Hau <virtualda@gmail.com>
2022-02-02 09:33:10 +1100

    Updated useful-tools/git/run_git_pull.sh

--- useful-tools/git/run_git_pull.sh
+++ useful-tools/git/run_git_pull.sh
@@ -2,7 +2,7 @@
 
 run_git_commands() {
   git pull
-  git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks fetch --prune origin
+  git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks fetch --prune --tags origin
 }
 
 for dir in $(find . -name .git -type d -prune); do
