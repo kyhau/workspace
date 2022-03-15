4dd006bcd46bf243cb2b26e940f040b78deba552
Kay Hau <virtualda@gmail.com>
2022-03-15 20:01:50 +1100

    Create install-lens.ps1

---
+++ useful-tools/kubernetes/install-lens.ps1
@@ -0,0 +1,5 @@
+choco install -y lens
+
+Write-Host "TODO: Run > aws eks update-kubeconfig --name cluster_name"
+
+Write-Host "TODO: Start Lens > File > Preference > Kubernetes > Kubeconfig Syncs > (select HOME\.kube)"
