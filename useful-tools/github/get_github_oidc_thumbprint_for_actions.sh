313284e777fc5066a24d1513509950a60af86d3c
Kay <virtualda@gmail.com>
2022-09-16 12:11:46 +1000

    Minor cleanup (#54)

--- useful-tools/github/get_github_oidc_thumbprint_for_actions.sh
+++ useful-tools/github/get_github_oidc_thumbprint_for_actions.sh
@@ -18,6 +18,4 @@ echo Approach 2
 HOST=$(curl -s https://vstoken.actions.githubusercontent.com/.well-known/openid-configuration | jq -r '.jwks_uri | split("/")[2]')
 echo | openssl s_client -servername $HOST -showcerts -connect $HOST:443 2> /dev/null \
 | sed -n -e '/BEGIN/h' -e '/BEGIN/,/END/H' -e '$x' -e '$p' | tail +2 \
-| openssl x509 -fingerprint -noout \
-| sed -e "s/.*=//" -e "s/://g" \
-| tr '[:upper:]' '[:lower:]'
+| openssl x509 -fingerprint -noout | sed -e "s/.*=//" -e "s/://g" | tr '[:upper:]' '[:lower:]'
