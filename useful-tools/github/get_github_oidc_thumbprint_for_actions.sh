2933e2084e165b1f546587f47b63ddb2607cd715
Kay Hau <virtualda@gmail.com>
2022-09-16 18:20:03 +1000

    Updated

--- useful-tools/github/get_github_oidc_thumbprint_for_actions.sh
+++ useful-tools/github/get_github_oidc_thumbprint_for_actions.sh
@@ -6,4 +6,3 @@ PROVIDER_URL="token.actions.githubusercontent.com"
 echo | openssl s_client -servername ${PROVIDER_URL} -showcerts -connect ${PROVIDER_URL}:443 2> /dev/null \
 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" \
 | openssl x509 -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]'
-
