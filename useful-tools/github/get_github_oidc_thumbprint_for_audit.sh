a51e3eddb09451ebf7632868393d5bb89b150b62
Kay <virtualda@gmail.com>
2022-09-16 10:36:34 +1000

    Updated github scripts (#50)

--- useful-tools/github/get_github_oidc_thumbprint_for_audit.sh
+++ useful-tools/github/get_github_oidc_thumbprint_for_audit.sh
@@ -5,6 +5,6 @@ set -e
 # Json Web Key Service URI https://oidc-configuration.audit-log.githubusercontent.com/.well-known/jwks
 
 openssl s_client -servername oidc-configuration.audit-log.githubusercontent.com -showcerts -connect oidc-configuration.audit-log.githubusercontent.com:443 < /dev/null 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" > certificate.crt
-openssl x509 -in certificate.crt -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]'
+openssl x509 -in certificate.crt -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:lower:]' '[:upper:]'
 
 rm certificate.crt
