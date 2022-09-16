98c984f5922a55667793fb80cd9f2d25cf722819
Kay Hau <virtualda@gmail.com>
2022-09-16 10:39:22 +1000

    Updated github scripts

--- useful-tools/github/get_github_oidc_thumbprint_for_actions.sh
+++ useful-tools/github/get_github_oidc_thumbprint_for_actions.sh
@@ -8,7 +8,7 @@ echo Approach 1
 # https://gist.github.com/guitarrapc/8e6b68f21bc1eef8e7b66bde477d5859?permalink_comment_id=4027755#file-_get_github_oidc_thumbprint-sh
 
 openssl s_client -servername token.actions.githubusercontent.com -showcerts -connect token.actions.githubusercontent.com:443 < /dev/null 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" > certificate.crt
-openssl x509 -in certificate.crt -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr  '[:lower:]' '[:upper:]'
+openssl x509 -in certificate.crt -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]'
 
 rm certificate.crt
 
@@ -20,4 +20,4 @@ echo | openssl s_client -servername $HOST -showcerts -connect $HOST:443 2> /dev/
 | sed -n -e '/BEGIN/h' -e '/BEGIN/,/END/H' -e '$x' -e '$p' | tail +2 \
 | openssl x509 -fingerprint -noout \
 | sed -e "s/.*=//" -e "s/://g" \
-| tr '[:lower:]' '[:upper:]'
+| tr '[:upper:]' '[:lower:]'
