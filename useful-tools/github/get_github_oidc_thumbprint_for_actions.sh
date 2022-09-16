ee279ff1d3fa82b60d1db6180a152b6b2fe0c72b
Kay <virtualda@gmail.com>
2022-09-16 18:19:07 +1000

    Updated oidc thumbprint script (#55)

--- useful-tools/github/get_github_oidc_thumbprint_for_actions.sh
+++ useful-tools/github/get_github_oidc_thumbprint_for_actions.sh
@@ -1,21 +1,9 @@
-#!/bin/bash
-set -e
-
 # OpenID Configuration URI https://token.actions.githubusercontent.com/.well-known/openid-configuration
 # Json Web Key Service URI https://token.actions.githubusercontent.com/.well-known/jwks
 
-echo Approach 1
-# https://gist.github.com/guitarrapc/8e6b68f21bc1eef8e7b66bde477d5859?permalink_comment_id=4027755#file-_get_github_oidc_thumbprint-sh
+PROVIDER_URL="token.actions.githubusercontent.com"
 
-openssl s_client -servername token.actions.githubusercontent.com -showcerts -connect token.actions.githubusercontent.com:443 < /dev/null 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" > certificate.crt
-openssl x509 -in certificate.crt -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]'
+echo | openssl s_client -servername ${PROVIDER_URL} -showcerts -connect ${PROVIDER_URL}:443 2> /dev/null \
+| sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" \
+| openssl x509 -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]'
 
-rm certificate.crt
-
-echo Approach 2
-# https://stackoverflow.com/questions/69247498/how-can-i-calculate-the-thumbprint-of-an-openid-connect-server
-
-HOST=$(curl -s https://vstoken.actions.githubusercontent.com/.well-known/openid-configuration | jq -r '.jwks_uri | split("/")[2]')
-echo | openssl s_client -servername $HOST -showcerts -connect $HOST:443 2> /dev/null \
-| sed -n -e '/BEGIN/h' -e '/BEGIN/,/END/H' -e '$x' -e '$p' | tail +2 \
-| openssl x509 -fingerprint -noout | sed -e "s/.*=//" -e "s/://g" | tr '[:upper:]' '[:lower:]'
--- useful-tools/github/get_github_oidc_thumbprint_for_audit_log.sh
+++ useful-tools/github/get_github_oidc_thumbprint_for_audit_log.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-set -e
-
 # OpenID Configuration URI https://oidc-configuration.audit-log.githubusercontent.com/.well-known/openid-configuration
 # Json Web Key Service URI https://oidc-configuration.audit-log.githubusercontent.com/.well-known/jwks
 
-openssl s_client -servername oidc-configuration.audit-log.githubusercontent.com -showcerts -connect oidc-configuration.audit-log.githubusercontent.com:443 < /dev/null 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" > certificate.crt
-openssl x509 -in certificate.crt -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]'
+PROVIDER_URL="oidc-configuration.audit-log.githubusercontent.com"
 
-rm certificate.crt
+echo | openssl s_client -servername ${PROVIDER_URL} -showcerts -connect ${PROVIDER_URL}:443 2> /dev/null \
+| sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" \
+| openssl x509 -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]'
