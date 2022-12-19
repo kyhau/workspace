a01e84a538afe1aa930f04fde4eb8ecf519fa7c9
Kay <virtualda@gmail.com>
2022-12-19 13:47:03 +1100

    Update useful-tools/python/run_pip_upgrade_all.sh (#90)

--- useful-tools/python/run_pip_upgrade_all.sh
+++ useful-tools/python/run_pip_upgrade_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pip install -U pip
-pip list --outdated --format=freeze | grep -v '^\-e' | grep -vi PyGObject | cut -d = -f 1  | xargs -n1 pip install -U
+pip list --outdated --exclude PyGObject | grep -v '^Package ' | grep -v '^\-' | cut -d ' ' -f 1  | xargs -n1 pip install -U
 
 #pip freeze > tmp_requirements.txt && pip install -U -r tmp_requirements.txt && rm tmp_requirements.txt
