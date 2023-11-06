8c8049d180832bd60248e05d7e676a34ebaaf17e
Kay Hau <virtualda@gmail.com>
2023-11-06 20:39:05 +1100

    Updated .aliases

--- useful-tools/sec/install_trufflehog.sh
+++ useful-tools/sec/install_trufflehog.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p ${HOME}/.local/bin
 
 if [ -x "$(command -v trufflehog)" ]; then
-  echo "INFO: trufflehog version: $(trufflehog --version)"
+  trufflehog --version
 else
   echo "INFO: trufflehog not installed"
 fi
@@ -13,4 +13,4 @@ fi
 echo "INFO: Installing trufflehog to ${HOME}/.local/bin"
 curl -sSfL https://raw.githubusercontent.com/trufflesecurity/trufflehog/main/scripts/install.sh | sh -s -- -b ${HOME}/.local/bin
 
-echo "INFO: trufflehog version: $(trufflehog --version)"
+trufflehog --version
