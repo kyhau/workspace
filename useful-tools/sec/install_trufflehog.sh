77aa0d62e4f8ff3c9cde8c5c2de39841f0d0ff39
Kay Hau <virtualda@gmail.com>
2023-11-11 13:10:11 +1100

    Updated useful-tools/sec/install_trufflehog.sh

--- useful-tools/sec/install_trufflehog.sh
+++ useful-tools/sec/install_trufflehog.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-set -e
 # https://github.com/trufflesecurity/trufflehog
+set -e
 
-mkdir -p ${HOME}/.local/bin
+VERSION=$(curl -s "https://api.github.com/repos/trufflesecurity/trufflehog/releases/latest" --insecure | grep -Po '"tag_name": "\K.*?(?=")' | sed 's/^v//')
+echo "INFO: trufflehog latest: ${VERSION}"
 
+CURR_VERSION=
 if [ -x "$(command -v trufflehog)" ]; then
-  trufflehog --version
+  CURR_VERSION=$(trufflehog --version 2>&1 | awk '{print $2}')
+  echo "INFO: trufflehog installed: ${CURR_VERSION}"
 else
   echo "INFO: trufflehog not installed"
 fi
 
-echo "INFO: Installing trufflehog to ${HOME}/.local/bin"
-curl -sSfL https://raw.githubusercontent.com/trufflesecurity/trufflehog/main/scripts/install.sh | sh -s -- -b ${HOME}/.local/bin
+if [ "${VERSION}" != "${CURR_VERSION}" ]; then
+  echo "INFO: Installing to ${HOME}/.local/bin/"
+  mkdir -p ${HOME}/.local/bin
+  curl -sSfL https://raw.githubusercontent.com/trufflesecurity/trufflehog/main/scripts/install.sh | sh -s -- -b ${HOME}/.local/bin
 
-trufflehog --version
+  echo "INFO: trufflehog installed: $(trufflehog --version 2>&1 | awk '{print $2}')"
+fi
