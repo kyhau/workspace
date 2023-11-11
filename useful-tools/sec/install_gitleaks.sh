36531b93bbe8463a001422cc1b4c1e2cc27154b8
Kay Hau <virtualda@gmail.com>
2023-11-11 12:39:22 +1100

    Added useful-tools/sec/install_gitleaks.sh

---
+++ useful-tools/sec/install_gitleaks.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# See https://github.com/gitleaks/gitleaks
+
+VERSION=$(curl -s "https://api.github.com/repos/gitleaks/gitleaks/releases/latest" --insecure | grep -Po '"tag_name": "\K.*?(?=")' | sed 's/^v//')
+echo "INFO: gitleaks latest: ${VERSION}"
+
+CURR_VERSION=
+if [ -x "$(command -v gitleaks)" ]; then
+  CURR_VERSION=$(gitleaks version 2>&1)
+  echo "INFO: gitleaks installed: ${CURR_VERSION}"
+else
+  echo "INFO: gitleaks not installed"
+fi
+
+if [ "${VERSION}" != "${CURR_VERSION}" ]; then
+  echo "INFO: Installing to ${HOME}/.local/bin/"
+
+  FILE_NAME=gitleaks_${VERSION}_linux_x64.tar.gz
+  FILE_URL="https://github.com/gitleaks/gitleaks/releases/download/v${VERSION}/${FILE_NAME}"
+  WORK_DIR=/tmp/tmp_workdir
+
+  wget ${FILE_URL} -O /tmp/${FILE_NAME} --no-check-certificate -q
+  mkdir -p ${WORK_DIR}
+  tar xfz /tmp/${FILE_NAME} -C ${WORK_DIR}
+
+  mkdir -p ${HOME}/.local/bin
+  mv ${WORK_DIR}/gitleaks ${HOME}/.local/bin/
+
+  rm -rf ${WORK_DIR}
+
+  echo "INFO: gitleaks installed: $(gitleaks version 2>&1)"
+fi
