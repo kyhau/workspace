15f95d78a5e408a3b8fc922610e16ec9576f6932
Kay <virtualda@gmail.com>
2024-09-27 20:42:28 +1000

    Added macos support (#176)

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # Quick setup for my development environment 💻
 
+## macOS
+- Install Homebrew and some packages with [macos/init-setup-macos.sh](macos/init-setup-macos.sh).
+    - [macos/install-homebrew.sh](macos/install-homebrew.sh), if do not have admin right, install things in `${HOME}/Applications/` instead.
+    - [macos/install-or-upgrade-apps-macos.sh](macos/install-or-upgrade-apps-macos.sh).
+- Set up aliases - [aliases-macos](.aliases-macos).
+- Docker:
+    - Option 1: Install Docker Desktop with dmg from official website.
+    - Option 2: Just install Docker, with `brew install docker`.
+
+## Windows + WSL
 - Set up Windows - install [Chocolatey](https://chocolatey.org) and some packages with [Init-chocolatey-windows.bat](./windows/chocolatey/Init-chocolatey-windows.bat)
 - [Set up WSL Ubuntu dev environment](./WSL.md)
 - [Set up Docker Desktop WSL](./Docker.md)
 - [Set up aliases](./.aliases)
-- Set up VS Code with my settings and user snippets in [kyhau/vscode-configs](https://github.com/kyhau/vscode-configs)
-- Set up `git` config for [multiple GitHub accounts](https://blog.gitguardian.com/8-easy-steps-to-set-up-multiple-git-accounts/)
-- Set up `gh` (GitHub CLI) [multi-account switch](https://gist.github.com/yermulnik/017837c01879ed3c7489cc7cf749ae47)
 
 ## Set up tools for specific development work
 
-- [Useful tools for development](./useful-tools/)
+- Set up VS Code with my settings and user snippets in [kyhau/vscode-configs](https://github.com/kyhau/vscode-configs)
+- Set up `git` config for [multiple GitHub accounts](https://blog.gitguardian.com/8-easy-steps-to-set-up-multiple-git-accounts/)
+- Set up `gh` (GitHub CLI) [multi-account switch](https://gist.github.com/yermulnik/017837c01879ed3c7489cc7cf749ae47)
+- Some of my scripts for installing [Useful tools for development](./useful-tools/)
 - [kyhau/aws-tools](https://github.com/kyhau/aws-tools/) - my tools/scripts for building with AWS
 
 ## Locations of some useful files or directories
@@ -23,4 +33,4 @@
 - WSL 1 Ubuntu home: `%USERPROFILE%\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_xxx\LocalState\rootfs\home\username`
 - WSL 2 Ubuntu home: `\\wsl$\Ubuntu`
 
-## [Some useful links](./quick-notes/UsefulLinks.md)
+### [Some useful links](./quick-notes/UsefulLinks.md)
---
+++ macos/init-setup-macos.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Set to fail script if any command fails.
+set -e
+
+WORKSPACES=${HOME}/Workspaces
+WORKSPACE_REPO=${WORKSPACES}/github/workspace
+CONFIGS_REPO=${CONFIGS_REPO}
+
+# Prequisites
+# Need these repos to be cloned first
+echo "CheckPt $((step++)): Check ${WORKSPACE_REPO}"
+[[ -e "${WORKSPACE_REPO}" ]] || { echo "${WORKSPACE_REPO} not exist - aborted"; return; }
+
+echo "CheckPt $((step++)): Check ${CONFIGS_REPO}"
+[[ -e "${CONFIGS_REPO}" ]] || { echo "${CONFIGS_REPO} not exist - aborted"; return; }
+
+step=1
+
+setup_homebrew() {
+  mkdir -p ${HOME}/Applications
+  echo "CheckPt $((step++)): Install Homebrew"
+  . ${WORKSPACE_REPO}/macos/install-homebrew.sh
+
+  brew analytics off
+}
+# setup_homebrew
+
+install_apps_with_brew() {
+  echo "CheckPt $((step++)): Install apps with brew"
+  . ${WORKSPACE_REPO}/macos/install-or-upgrade-apps.sh
+}
+# install_apps_with_brew
+
+setup_workspace() {
+
+  ################################################################################
+  echo "CheckPt $((step++)): Set up .zshrc"
+  cp ${CONFIGS_REPO}/.zshrc ~/.zshrc
+
+  ################################################################################
+  echo "CheckPt $((step++)): Set up .aws"
+  [[ -d "${HOME}/.aws" ]] || mkdir -p ~/.aws
+  cp ${CONFIGS_REPO}/.aws/config ~/.aws/config
+
+  ################################################################################
+  echo "CheckPt $((step++)): Set up .config"
+  [[ -d "${HOME}/.config" ]] || mkdir -p ~/.config
+
+  mkdir -p ~/.config/gh
+  cp ${CONFIGS_REPO}/.config/gh/config.yml ~/.config/gh/config.yml
+
+  mkdir -p ~/.config/pip
+  cp ${CONFIGS_REPO}/.config/pip/pip.conf ~/.config/pip/pip.conf
+
+  ################################################################################
+  echo "CheckPt $((step++)): Set up .ssh"
+  [[ -d "${HOME}/.ssh" ]] || mkdir -p ${HOME}/.ssh
+  cp ${CONFIGS_REPO}/.ssh/config ~/.ssh/config
+
+  ################################################################################
+  echo "CheckPt $((step++)): Set up .git configs"
+  cp ${CONFIGS_REPO}/.gitconfig-macos ~/.gitconfig
+  cp ${CONFIGS_REPO}/.gitconfig-gh-k ~/.gitconfig-gh-k
+  cp ${CONFIGS_REPO}/.gitconfig-gh-w ~/.gitconfig-gh-w
+  cp ${CONFIGS_REPO}/.gitignore ~/.gitignore
+
+  git config --global core.excludesFile '~/.gitignore'
+
+  ################################################################################
+  source ~/workspaces/github/workspace/.aliases
+}
+# setup_workspace
+
+setup_dev () {
+  ################################################################################
+  echo "CheckPt $((step++)): Set up saml2aws-multi"
+  cd ${WORKSPACES}/github/w
+  git clone https://github.com/kyhau/saml2aws-multi
+  cd saml2aws-multi
+  pip install -e .
+  cd -
+
+  ################################################################################
+  echo "CheckPt $((step++)): Set up vscode-configs"
+  cd ${WORKSPACES}/github/w
+  git clone https://github.com/kyhau/vscode-configs
+  cd -
+}
