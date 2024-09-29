62595b9696726e4f0279d12653a05f3e33f571b0
Kay Hau <virtualda@gmail.com>
2024-09-29 15:32:12 +1000

    Updated useful-tools/oh-my-zsh/install-oh-my-zsh.macos.sh

--- useful-tools/oh-my-zsh/install-oh-my-zsh.macos.sh
+++ useful-tools/oh-my-zsh/install-oh-my-zsh.macos.sh
@@ -1,3 +1,5 @@
 # https://ohmyz.sh/#install
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10ke
